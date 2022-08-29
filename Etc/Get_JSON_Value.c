968ba11eb9aff5df6151c2ebac962ab6b4b33c34
EomYerim <djadpfa1002@naver.com>
2022-08-29 23:44:27 +0900

    feat: Returns if key value exists in JSON

---
+++ Etc/Get_JSON_Value.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "cJSON.h"
+#include "onem2m.h"
+
+Get_JSON_Value(char *key, char *json) {
+	char json_copy[100];
+	char *resource = NULL;
+	char *value = NULL;
+
+	cJSON *root = NULL;
+	cJSON *ckey = NULL;
+
+	cJSON *cjson = cJSON_Parse(json);
+	if (cjson == NULL) {
+		const char *error_ptr = cJSON_GetErrorPtr();
+		if (error_ptr != NULL)
+		{
+			fprintf(stderr, "Error before: %s\n", error_ptr);
+		}
+		goto end;
+	}
+
+	//Extracting resources from json
+	strcpy(json_copy, json);
+	resource = strstr(json_copy, "m2m:");
+	resource = strtok(resource, "\"");
+
+	root = cJSON_GetObjectItem(cjson, resource);
+
+	ckey = cJSON_GetObjectItem(root, key);
+	if (!cJSON_IsString(ckey) && ckey->valuestring == NULL) {
+		goto end;
+	}
+	value = cJSON_Print(ckey);
+	value = strtok(value, "\"");
+
+end:
+	cJSON_Delete(cjson);
+
+	return value;
+}
