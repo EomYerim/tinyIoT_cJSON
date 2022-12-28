c1aa705949f378a378c41e5d99fbb5ec38b1e3e2
EomYerim <djadpfa1002@naver.com>
2022-12-29 01:17:00 +0900

    fix: Optional attribute exception handling

--- CNT/JSON_to_CNT.c
+++ CNT/JSON_to_CNT.c
@@ -2,10 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 #include "cJSON.h"
 #include "onem2m.h"
 
-CNT* Json_to_CNT(char *json_payload) {
+CNT* JSON_to_CNT(char *json_payload) {
 	CNT *cnt = (CNT *)malloc(sizeof(CNT));
 
 	cJSON *root = NULL;
@@ -24,27 +25,50 @@ CNT* Json_to_CNT(char *json_payload) {
 
 	root = cJSON_GetObjectItem(json, "m2m:cnt");
 
-	// rn
+	// rn (optional)
 	rn = cJSON_GetObjectItem(root, "rn");
-	if (!cJSON_IsString(rn) && (rn->valuestring == NULL))
-	{
-		goto end;
+	if (rn == NULL || isspace(rn->valuestring[0])) {
+		cnt->rn = NULL;
 	}
-	cnt->rn = cJSON_Print(rn);
-	cnt->rn = strtok(cnt->rn, "\"");
+	else {
+		cnt->rn = cJSON_Print(rn);
+		cnt->rn = strtok(cnt->rn, "\"");
+	}
 
-	// acpi
+
+	// acpi (optional)
 	acpi = cJSON_GetObjectItem(root, "acpi");
-	int acpi_size = cJSON_GetArraySize(acpi);
-	char acpi_str[100] = { '\0' };
-	for (int i = 0; i < acpi_size; i++) {
-		strcat(acpi_str, cJSON_GetArrayItem(acpi, i)->valuestring);
-		if (i < acpi_size - 1) {
-			strcat(acpi_str, ",");
+	if (acpi == NULL) {
+		cnt->acpi = NULL;
+	}
+	else {
+		int acpi_size = cJSON_GetArraySize(acpi);
+		int is_NULL_flag = 0;
+		if (acpi_size == 0) {
+			cnt->acpi = NULL;
+		}
+		else {
+			char acpi_str[100] = { '\0' };
+			for (int i = 0; i < acpi_size; i++) {
+				if (isspace(cJSON_GetArrayItem(acpi, i)->valuestring[0]) || (cJSON_GetArrayItem(acpi, i)->valuestring[0] == 0)) {
+					cnt->acpi = NULL;
+					is_NULL_flag = 1;
+					break;
+				}
+				else {
+					strcat(acpi_str, cJSON_GetArrayItem(acpi, i)->valuestring);
+					if (i < acpi_size - 1) {
+						strcat(acpi_str, ",");
+					}
+				}
+			}
+			if (is_NULL_flag != 1) {
+				cnt->acpi = (char *)malloc(sizeof(char *) * strlen(acpi_str) + 1);
+				strcpy(cnt->acpi, acpi_str);
+			}
 		}
 	}
-	cnt->acpi = (char *)malloc(sizeof(char *) * strlen(acpi_str) + 1);
-	strcpy(cnt->acpi, acpi_str);
+
 
 end:
 	cJSON_Delete(json);
