367caa5001076dfbd0d5c7689001cfc1417f0662
EomYerim <djadpfa1002@naver.com>
2022-12-29 01:18:43 +0900

    fix: Mandatory/Optional attribute exception handling

--- CIN/JSON_to_CIN.c
+++ CIN/JSON_to_CIN.c
@@ -2,19 +2,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
+#include <ctype.h>
 #include "cJSON.h"
 #include "onem2m.h"
 
-CIN* Json_to_CIN(char *json_payload) {
+CIN* JSON_to_CIN(char *json_payload) {
 	CIN *cin = (CIN *)malloc(sizeof(CIN));
 
 	cJSON *root = NULL;
 	cJSON *rn = NULL;
 	cJSON *con = NULL;
 
+	const char *error_ptr = NULL;
+
 	cJSON* json = cJSON_Parse(json_payload);
 	if (json == NULL) {
-		const char *error_ptr = cJSON_GetErrorPtr();
+		error_ptr = cJSON_GetErrorPtr();
 		if (error_ptr != NULL)
 		{
 			fprintf(stderr, "Error before: %s\n", error_ptr);
@@ -24,26 +27,26 @@ CIN* Json_to_CIN(char *json_payload) {
 
 	root = cJSON_GetObjectItem(json, "m2m:cin");
 
-	// rn
-	rn = cJSON_GetObjectItem(root, "rn");
-	if (!cJSON_IsString(rn) && (rn->valuestring == NULL))
-	{
-		goto end;
-	}
-	cin->rn = cJSON_Print(rn);
-	cin->rn = strtok(cin->rn, "\"");
-
-	// con
+	// con (mandatory)
 	con = cJSON_GetObjectItem(root, "con");
-	if (!cJSON_IsString(con) && (con->valuestring == NULL))
+	if (con == NULL || con->valuestring[0] == 0 || isspace(con->valuestring[0]))
 	{
+		fprintf(stderr, "Invalid con\n");
 		goto end;
 	}
 	cin->con = cJSON_Print(con);
 	cin->con = strtok(cin->con, "\"");
 
-	// cs
-	cin->cs = strlen(cin->con);
+
+	// rn (optional)
+	rn = cJSON_GetObjectItem(root, "rn");
+	if (rn == NULL || isspace(rn->valuestring[0])) {
+		cin->rn = NULL;
+	}
+	else {
+		cin->rn = cJSON_Print(rn);
+		cin->rn = strtok(cin->rn, "\"");
+	}
 
 end:
 	cJSON_Delete(json);
