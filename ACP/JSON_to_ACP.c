1de152a7d433c9094b2fa3d03b9ff252f17cca43
EomYerim <djadpfa1002@naver.com>
2022-12-29 01:26:42 +0900

    fix: Mandatory/Optional attribute exception handling

--- ACP/JSON_to_ACP.c
+++ ACP/JSON_to_ACP.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include "cJSON.h"
 #include "onem2m.h"
 
@@ -28,91 +29,155 @@ ACP* JSON_to_ACP(char *json_payload) {
 
 	root = cJSON_GetObjectItem(json, "m2m:acp");
 
-	// rn
-	rn = cJSON_GetObjectItem(root, "rn");
-	if (!cJSON_IsString(rn) && rn->valuestring == NULL)
-	{
-		goto end;
-	}
-	acp->rn = cJSON_Print(rn);
-	acp->rn = strtok(acp->rn, "\"");
-
 	// pv
 	pv = cJSON_GetObjectItem(root, "pv");
-
-	// acr (접근 제어 규칙)
-	acrs = cJSON_GetObjectItem(pv, "acr");
-	int acr_size = cJSON_GetArraySize(acrs);
-	char acor_str[100] = { '\0' };
-	char acop_str[100] = { '\0' };
-	int i = 0;
-	cJSON_ArrayForEach(acr, acrs) {
-		acor = cJSON_GetObjectItem(acr, "acor");
-		int acor_size = cJSON_GetArraySize(acor);
-		for (int j = 0; j < acor_size; j++) {
-			strcat(acor_str, cJSON_GetArrayItem(acor, j)->valuestring);
-			if (j < acor_size - 1) {
-				strcat(acor_str, ",");
-			}
-		}
-		if (i < acr_size - 1)
-			strcat(acor_str, ",");
-
-		acop = cJSON_GetObjectItem(acr, "acop");
-		for (int j = 0; j < acor_size; j++) {
-			strcat(acop_str, strtok(cJSON_Print(acop), "\""));
-			if (j < acor_size - 1) {
-				strcat(acop_str, ",");
-			}
-		}
-		if (i < acr_size - 1)
-			strcat(acop_str, ",");
-		i++;
+	if (pv == NULL) {
+		fprintf(stderr, "Invalid pv\n");
+		goto end;
 	}
-	acp->pv_acor = (char *)malloc(sizeof(char) * strlen(acor_str) + 1);
-	strcpy(acp->pv_acor, acor_str);
+	else {
+		// acr
+		acrs = cJSON_GetObjectItem(pv, "acr");
+		int acr_size = cJSON_GetArraySize(acrs);
+		if (acrs == NULL || acr_size == 0) {
+			fprintf(stderr, "Invalid pv-acr\n");
+			goto end;
+		}
+		else {
+			char acor_str[100] = { '\0' };
+			char acop_str[100] = { '\0' };
+			int i = 0;
+			cJSON_ArrayForEach(acr, acrs) {
+				acor = cJSON_GetObjectItem(acr, "acor");
+				int acor_size = cJSON_GetArraySize(acor);
+				if (acor == NULL || acor_size == 0) {
+					fprintf(stderr, "Invalid pv-acr-acor\n");
+					if (acor_size == 0) {
+						fprintf(stderr, "Invalid pv-acr-acop\n");
+					}
+					goto end;
+				}
+				else {
+					for (int j = 0; j < acor_size; j++) {
+						if (isspace(cJSON_GetArrayItem(acor, j)->valuestring[0]) || (cJSON_GetArrayItem(acor, j)->valuestring[0] == 0)) {
+							fprintf(stderr, "Invalid pv-acr-acor\n");
+							goto end;
+						}
+						else {
+							strcat(acor_str, cJSON_GetArrayItem(acor, j)->valuestring);
+							if (j < acor_size - 1) {
+								strcat(acor_str, ",");
+							}
+						}
+					}
+					if (i < acr_size - 1)
+						strcat(acor_str, ",");
+				}
 
-	acp->pv_acop = (char *)malloc(sizeof(char) * strlen(acop_str) + 1);
-	strcpy(acp->pv_acop, acop_str);
+				acop = cJSON_GetObjectItem(acr, "acop");
+				if (acop == NULL || acop->valuestring[0] == 0 || isspace(acop->valuestring[0])) {
+					fprintf(stderr, "Invalid pv-acr-acop\n");
+					goto end;
+				}
+				else {
+					for (int j = 0; j < acor_size; j++) {
+						strcat(acop_str, strtok(cJSON_Print(acop), "\""));
+						if (j < acor_size - 1) {
+							strcat(acop_str, ",");
+						}
+					}
+					if (i < acr_size - 1)
+						strcat(acop_str, ",");
+					i++;
+				}
+			}
+			acp->pv_acor = (char *)malloc(sizeof(char) * strlen(acor_str) + 1);
+			strcpy(acp->pv_acor, acor_str);
 
+			acp->pv_acop = (char *)malloc(sizeof(char) * strlen(acop_str) + 1);
+			strcpy(acp->pv_acop, acop_str);
+		}
+	}
 
 	// pvs
 	pvs = cJSON_GetObjectItem(root, "pvs");
-
-	// acr
-	acrs = cJSON_GetObjectItem(pvs, "acr");
-	acr_size = cJSON_GetArraySize(acrs);
-	memset(acor_str, 0, 100);	// 배열 재 초기화
-	memset(acop_str, 0, 100);
-	i = 0;
-	cJSON_ArrayForEach(acr, acrs) {
-		acor = cJSON_GetObjectItem(acr, "acor");
-		int acor_size = cJSON_GetArraySize(acor);
-		for (int j = 0; j < acor_size; j++) {
-			strcat(acor_str, cJSON_GetArrayItem(acor, j)->valuestring);
-			if (j < acor_size - 1) {
-				strcat(acor_str, ",");
-			}
-		}
-		if (i < acr_size - 1)
-			strcat(acor_str, ",");
-
-		acop = cJSON_GetObjectItem(acr, "acop");
-		for (int j = 0; j < acor_size; j++) {
-			strcat(acop_str, strtok(cJSON_Print(acop), "\""));
-			if (j < acor_size - 1) {
-				strcat(acop_str, ",");
-			}
-		}
-		if (i < acr_size - 1)
-			strcat(acop_str, ",");
-		i++;
+	if (pvs == NULL) {
+		fprintf(stderr, "Invalid pvs\n");
+		goto end;
 	}
-	acp->pvs_acor = (char *)malloc(sizeof(char) * strlen(acor_str) + 1);
-	strcpy(acp->pvs_acor, acor_str);
+	else {
+		// acr
+		acrs = cJSON_GetObjectItem(pvs, "acr");
+		int acr_size = cJSON_GetArraySize(acrs);
+		if (acrs == NULL || acr_size == 0) {
+			fprintf(stderr, "Invalid pvs-acr\n");
+			goto end;
+		}
+		else {
+			char acor_str[100] = { '\0' };
+			char acop_str[100] = { '\0' };
+			int i = 0;
+			cJSON_ArrayForEach(acr, acrs) {
+				acor = cJSON_GetObjectItem(acr, "acor");
+				int acor_size = cJSON_GetArraySize(acor);
+				if (acor == NULL || acor_size == 0) {
+					fprintf(stderr, "Invalid pvs-acr-acor\n");
+					if (acor_size == 0) {
+						fprintf(stderr, "Invalid pvs-acr-acop\n");
+					}
+					goto end;
+				}
+				else {
+					for (int j = 0; j < acor_size; j++) {
+						if (isspace(cJSON_GetArrayItem(acor, j)->valuestring[0]) || (cJSON_GetArrayItem(acor, j)->valuestring[0] == 0)) {
+							fprintf(stderr, "Invalid pvs-acr-acor\n");
+							goto end;
+						}
+						else {
+							strcat(acor_str, cJSON_GetArrayItem(acor, j)->valuestring);
+							if (j < acor_size - 1) {
+								strcat(acor_str, ",");
+							}
+						}
+					}
+					if (i < acr_size - 1)
+						strcat(acor_str, ",");
+				}
 
-	acp->pvs_acop = (char *)malloc(sizeof(char) * strlen(acop_str) + 1);
-	strcpy(acp->pvs_acop, acop_str);
+				acop = cJSON_GetObjectItem(acr, "acop");
+				if (acop == NULL || acop->valuestring[0] == 0 || isspace(acop->valuestring[0])) {
+					fprintf(stderr, "Invalid pvs-acr-acop\n");
+					goto end;
+				}
+				else {
+					for (int j = 0; j < acor_size; j++) {
+						strcat(acop_str, strtok(cJSON_Print(acop), "\""));
+						if (j < acor_size - 1) {
+							strcat(acop_str, ",");
+						}
+					}
+					if (i < acr_size - 1)
+						strcat(acop_str, ",");
+					i++;
+				}
+			}
+			acp->pvs_acor = (char *)malloc(sizeof(char) * strlen(acor_str) + 1);
+			strcpy(acp->pvs_acor, acor_str);
+
+			acp->pvs_acop = (char *)malloc(sizeof(char) * strlen(acop_str) + 1);
+			strcpy(acp->pvs_acop, acop_str);
+		}
+	}
+
+	// rn (optional)
+	rn = cJSON_GetObjectItem(root, "rn");
+	if (rn == NULL || isspace(rn->valuestring[0])) {
+		acp->rn = NULL;
+	}
+	else {
+		acp->rn = cJSON_Print(rn);
+		acp->rn = strtok(acp->rn, "\"");
+	}
 
 end:
 	cJSON_Delete(json);
